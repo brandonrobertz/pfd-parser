908e1b05c65b0b67bd47b71d57674e9a848ee965
Brandon Roberts <brandon@bxroberts.org>
2025-04-25 14:38:06 -0700

    Support 278 Transaction files
    Added some tests and small changes to ensure that transactions in
    nominee 278s as well as 278 transactions (which have an extra column)
    get extracted.
    
    This isn't perfect as some of the PFDs are weird and the second part of
    the extra long header on the 278T gets pulled in as the first row.

--- test/test.js
+++ test/test.js
@@ -9,7 +9,9 @@ const filingPath = __dirname + '/data/';
 
 const integrityFiling = filingPath + 'Kushner, Jared.pdf',
     fdmFiling = filingPath + 'Donnelly, Sally.pdf',
-    fdOnlineFiling = filingPath + 'Mashburn, Lori K.pdf';
+    fdOnlineFiling = filingPath + 'Mashburn, Lori K.pdf',
+    transFiling = filingPath + 'Amy-A-Karpel-02.26.2025-278T.pdf',
+    filingWTransactions = filingPath + 'Archeval Anthony - HRSA - Annual  - 278 - 2024 - 2024-02-28.pdf';
 
 describe('lib/parser.js', () => {
     it('should find seven tables in Integrity filing', (done) => {
@@ -38,6 +40,22 @@ describe('lib/parser.js', () => {
                 done();
             });
     });
+    it('should find nine transactions in the Archeval example', (done) => {
+        parser(filingWTransactions)
+            .then((filings) => {
+                filings[0].tables[3].rows.length.should.equal(34);
+                Object.keys(filings[0].tables[3].rows[0]).length.should.equal(5);
+                done();
+            });
+    });
+    it('should find six transactions in the Karpel 278T PFD', (done) => {
+        parser(transFiling)
+            .then((filings) => {
+                console.log("LOL", filings[0].tables[0]);
+                // TODO: fix this the first row is always garbage
+                filings[0].tables[0].rows.length.should.equal(7);
+                Object.keys(filings[0].tables[0].rows[2]).length.should.equal(6);
+                done();
+            });
+    });
 });
-
-
